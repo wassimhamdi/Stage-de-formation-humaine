4e06540a2ac4960937050d8236e886523239e574
wassimhamdi <hamdi.wassim91@gmail.com>
2014-12-09 15:15:21 +0100

    initial commit

---
+++ courbe.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<link rel="stylesheet" href="style.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Accueil</title>
+</head>
+ 
+
+<body>
+ <header>
+                <div id="titre_principal">
+                    <img src="w7.jpg" alt="Logo" id="logo" />
+					<span id="date_heure" style="text-align:left"></span>
+						<script type="text/javascript" >window.onload = date_heure('date_heure');</script>
+                </div>
+                 
+                <nav>
+                    <ul>
+                        <li ><a href="" id="e"><strong>Accueil </strong></a></li>
+                        <li><a href="thermo.html" class="f">|  La thermographie |</a></li>
+						<li><a href="presentation.html" class="f">A propos de nous |</a></li>
+                        <li><a href="test.html" class="f">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+             
+            <div id="banniere_image">
+      				<div id="banniere_description">
+					Retrouver-nous aussi sur 
+					<a href="http://www.facebook.com" id="bouton_rouge"><img src="wass5.png" alt="" /></a>
+					<a href="http://www.twitter.com"><img src="t1.png" alt="" /></a>
+                    <marquee direction="left" scrollamount="4" width="677";>
+					<font face="Trebuchet MS" size="3" color="#FF00FF">VISirX  </font>
+					<font face="Verdana" size="2"> est une entreprise Tunisienne dont le principal domaine d’activité 
+					est la haute technologie (hi-tech) destinée au marché de contrôle non destructif par 
+					vision infrarouge thermique.</font>
+
+</marquee>                   
+                </div>
+            </div>
+
+<nav>
+<form action="" method="post">
+<label for="identifiant"> Identifiant </label>
+<br />
+<input type="text" name="identifiant"  id="indentifiant" />
+<br />
+<label for="password"> Password </label>
+<br />
+<input type="password" name="password"  id="password" />
+<br />
+
+<br />
+<input type="submit" value="se connecter" />
+<br />
+<p><a href="formulaire.php"> s'inscrire</a></p> 
+</form>
+</nav>
+</body> 
+</html>
